d9bb326191bb5549bbe47157358ec1cc84d40c28
Surajchhetry <surajchhetry@gmail.com>
2018-06-19 15:29:44 +0545

    finxed issue with Wildfly

--- src/main/java/ee/jakarta/mvc/controllers/IndexController.java
+++ src/main/java/ee/jakarta/mvc/controllers/IndexController.java
@@ -3,7 +3,9 @@ package ee.jakarta.mvc.controllers;
 import ee.jakarta.mvc.utils.ControllerUtils;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.mvc.MvcContext;
 import javax.mvc.annotation.Controller;
 import javax.mvc.annotation.View;
 import javax.ws.rs.FormParam;
@@ -11,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.logging.Logger;
 
 @Path("/")
@@ -21,17 +24,28 @@ public class IndexController {
 
     private Logger logger = Logger.getLogger(IndexController.class.getName());
 
+    private MvcContext mvcContext;
+
+    public IndexController() {
+    }
+
+    @Inject
+    public IndexController(MvcContext mvcContext) {
+        this.mvcContext = mvcContext;
+    }
+
     @GET
     @View("login.jsp")
     public void displayLoginView(){}
 
     @POST
     public Response onLogin(@FormParam("username") String username, @FormParam("password") String password){
-        return Response.ok(ControllerUtils.REDIRECT_TO + "home").build();
+        return Response.seeOther( URI.create(mvcContext.getApplicationPath()+"/home")).build();
+
     }
 
     @GET
-    @Path("/home")
+    @Path("home")
     @View("home.jsp")
     public void loadHomeView(){
     }
